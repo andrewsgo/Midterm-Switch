40e719a9d8048f726153d14f104abafd8c060b1f
Daniel Go <andrewsgo98@gmail.com>
2019-02-04 15:04:55 +0800

    placed partial color code

--- index.php
+++ index.php
@@ -1,8 +1,20 @@
 <!DOCTYPE html>
 <html>
 <body>
-    <?php?>
-    <form method="post" action="">
+    <?php
+    $favcolor = "red";
+
+switch ($favcolor) {
+    case "red":
+        echo "Your favorite color is red!";
+        break;
+    case "blue":
+        echo "Your favorite color is blue!";
+        break;
+    case "green":
+        echo "Your favorite color is green!";
+    ?>
+<form method="post" action="">
 <input type="text" name="value">
 <input type="submit">
 </form>
